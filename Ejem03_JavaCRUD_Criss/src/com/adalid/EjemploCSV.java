47fcb3110450e18660c13a0984e5e186a3a25b30
Criss <criss_sun_31@outlook.com>
2020-10-29 13:19:17 +0100

    Ejercicio CSV

---
+++ Ejem03_JavaCRUD_Criss/src/com/adalid/EjemploCSV.java
@@ -0,0 +1,39 @@
+package com.adalid;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.adalid.modelo.CRUD_2;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+public class EjemploCSV {
+
+	public static void cargarUsuarios(){
+		try {
+			CSVReader csvReader = new CSVReader(new FileReader("src/com/adalid/datos.csv"));
+			String[] listData;
+			CRUD_2 gesUsu = new CRUD_2();
+			
+			try {
+				while((listData = csvReader.readNext()) != null) {
+					for(String cell : listData) {
+						String[] csv = cell.split(",");
+						gesUsu.Create(csv[0],csv[1],csv[2],Integer.parseInt(csv[3]),Float.parseFloat(csv[4]),
+								Boolean.parseBoolean(csv[5]),csv[6].charAt(0));
+					}
+				}
+				gesUsu.Read();
+			} catch (CsvValidationException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
